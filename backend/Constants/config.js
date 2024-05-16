93846ad270c5713488ff8252ea62bacf22c142b1
weather-application <vinitparmar03@gmail.com>
2024-05-16 14:47:15 +0530

    change made in config.js in backend

--- backend/Constants/config.js
+++ backend/Constants/config.js
@@ -4,7 +4,7 @@ export const corsOptions = {
     "http://localhost:5172",
     "http://localhost:5174",
     "https://chatgram-1v1udvpsa-vinitparmar03s-projects.vercel.app",
-    "https://chatgram-git-master-vinitparmar03s-projects.vercel.app/",
+    "https://chatgram-git-master-vinitparmar03s-projects.vercel.app",
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
