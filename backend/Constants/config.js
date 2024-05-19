6fdb370cb2f485bf877ae0360408aa2d4acd96b0
weather-application <vinitparmar03@gmail.com>
2024-05-19 12:49:26 +0530

    updated config.js in backend

--- backend/Constants/config.js
+++ backend/Constants/config.js
@@ -1,15 +1,14 @@
 export const corsOptions = {
-  origin: true,
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5172",
+    "http://localhost:5174",
+    "https://chatgram-drab.vercel.app",
+    "https://chatgram-git-master-vinitparmar03s-projects.vercel.app",
+    "https://chatgram-ojpwije7m-vinitparmar03s-projects.vercel.app",
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
 
 export const CHATGRAM_TOKEN = "chatgram-token";
-
-// [
-//   "http://localhost:5173",
-//   "http://localhost:5172",
-//   "http://localhost:5174",
-//   "https://chatgram-1v1udvpsa-vinitparmar03s-projects.vercel.app",
-//   "https://chatgram-git-master-vinitparmar03s-projects.vercel.app",
-// ],
